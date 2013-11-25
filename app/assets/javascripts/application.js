71854c513ef73287df14b322c751c0d77e696f99
Conal <cms5109@gmail.com>
2013-11-25 15:03:37 -0500

    reordered JS, hopefully it loads

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bootstrap
 //= require jquery-1.9.1
 //= require jquery-ui
+//= require bootstrap
 //= require raphael
 //= require morris
 //= require slider
